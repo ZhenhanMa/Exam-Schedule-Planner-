d8d4cc128f831a13d3c558ff216ceceb3a002fd0
Cyanstep <xiatouxiaochun@gmail.com>
2026-09-04 19:49:30 +0800

    Delete github_upload directory

--- github_upload/electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-
-// 通过 contextBridge 安全地暴露 API 给渲染进程
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  isElectron: true,
-});
